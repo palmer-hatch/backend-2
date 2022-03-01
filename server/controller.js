d1ae1b8722f43431b5bbf120e9899f6f0b382512
Palmer-Hatch <56094431+palmermexico@users.noreply.github.com>
2022-03-01 16:09:44 -0700

    final

---
+++ server/controller.js
@@ -0,0 +1,48 @@
+const res = require('express/lib/response')
+const houses = require('./db.json')
+let globalID = 4
+
+
+
+module.exports = {
+    getHouses: (req, res) => {
+        res.status(200).send(houses)
+    },
+    createHouse: (req,res) => {
+       let newHouse = req.body
+       
+       newHouse.id = globalID
+       houses.push(newHouse)
+
+       res.status(200).send(houses)
+
+       globalID++
+
+    },
+    updateHouse: (req,res) => {
+        let {id} = req.params
+        let {type} = req.body
+
+        let index = houses.findIndex(elem => +elem.id === +id)
+
+        if(type === 'plus'){
+            houses[index].price += 10000
+            res.status(200).send(houses)
+        }else if (type === 'minus' && houses[index].price === 0 ) {
+            res.status(400).send('houses cannot go below $0')
+        } else if (type === 'minus'){
+            houses[index].price -= 10000
+            res.status(200).send(houses)
+        }
+        
+        else {
+            res.status(400).send(houses)
+        }
+
+    },
+    deleteHouse:(req,res)=> {
+        let index = houses.findIndex(house => +house.id === +req.params.id)
+        houses.splice(index,1)
+        res.status(200).send(houses)
+    }
+}
---
+++ server/index.js
@@ -0,0 +1,29 @@
+const res = require('express/lib/response')
+const express = require('express')
+const cors = require ('cors')
+
+
+const {getHouses, createHouse, updateHouse, deleteHouse} = require('./controller')
+
+
+// middleware
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+
+
+// endpoints
+app.get('/api/houses', getHouses)
+app.post('/api/houses', createHouse)
+app.put('/api/houses/:id', updateHouse)
+app.delete('/api/houses/:id', deleteHouse)
+
+
+
+
+
+
+
+
+app.listen(4004, () => console.log('server running on 4004'))
